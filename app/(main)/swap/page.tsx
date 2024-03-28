536c045a28dab67ee51ae99ab4dd71ece0cdc920
VertexViper <brilliantstar.tech@gmail.com>
2024-03-28 10:52:44 -0400

    fixed

--- app/(main)/swap/page.tsx
+++ app/(main)/swap/page.tsx
@@ -4,12 +4,14 @@ import { SwapWidget, Theme } from '@uniswap/widgets'
 import '@uniswap/widgets/fonts.css'
 import { useEffect } from 'react'
 import dynamic from 'next/dynamic'
+import Loader from '@/components/ui/loader'
 // import { JSON_RPC_URL } from '@/utils/constants'
 // import { InfuraProvider } from "@ethersproject/providers"
 
 // const provider = new InfuraProvider(1, "958524c46386411f8104c19bcd8f2903")
 
-const UniswapWidgetDynamic = dynamic(() => import('@/components/swap/swap'), {
+const UniswapWidgetDynamic = dynamic(() => import("@/components/portfolio/portfolioInfo"), {
+  loading: () => <Loader size={100} />,
   ssr: false,
 });
 
